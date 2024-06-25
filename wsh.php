6793794ff0b781bda66a64d481e2e8fa6c2f6e1b
Rodric <72948934+RodricBr@users.noreply.github.com>
2024-06-25 15:41:36 -0300

    Update wsh.php

--- wsh.php
+++ wsh.php
@@ -11,7 +11,7 @@ if (!empty($_POST['cmd'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="css/icone.png" rel="icon" type="image/x-icon">
   <link href="css/estilo.css" rel="stylesheet" type="text/css">
-  <title>WSH</title>
+  <title>sWSH</title>
 </head>
 
 <body>
